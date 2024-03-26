80353b1eea955c7c3b6a9955f0f167ffab8c1e2a
mahmud5658 <almahmuda5@gmail.com>
2024-03-27 02:52:30 +0600

    quick food

---
+++ lib/pages/bottomnav.dart
@@ -0,0 +1,68 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:quick_food_delivery/pages/home.dart';
+import 'package:quick_food_delivery/pages/order.dart';
+import 'package:quick_food_delivery/pages/profile.dart';
+import 'package:quick_food_delivery/pages/wallet.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int currentIndexTab = 0;
+  late List<Widget> pages;
+  late Widget currentPage;
+  late HomePage homePage;
+  late Wallet wallet;
+  late Profile profile;
+  late Order order;
+
+  @override
+  void initState() {
+    profile = Profile();
+    homePage = HomePage();
+    order = Order();
+    wallet = Wallet();
+    pages = [homePage, wallet, wallet, profile];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.black,
+        height: 65,
+        animationDuration: Duration(milliseconds: 500),
+        onTap: (int index) {
+          setState(() {
+            currentIndexTab = index;
+          });
+        },
+        items: const [
+          Icon(
+            Icons.home_outlined,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.shopping_bag_outlined,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.wallet_outlined,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.person_outline,
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: pages[currentIndexTab],
+    );
+  }
+}
